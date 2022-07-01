6abdf876dbf2b6f54b16e4fdc55dd585fed40e23
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-01 16:27:58 -0500

    agregando colores personalizados a tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      background: '#4b9195'
-    }
+    extend: {
+      colors:{
+        background: '#4b9195'
+      }
+    },
+    // colors:{
+    //   background: '#4b9195'
+    // }
   },
   plugins: [],
 }
