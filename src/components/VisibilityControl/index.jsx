42dfdf659d905aea876e8836790d3f33c5a9039f
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-02 21:58:45 -0500

    creando switch para mostrar tabla de completados

--- src/components/VisibilityControl/index.jsx
+++ src/components/VisibilityControl/index.jsx
@@ -1,4 +1,5 @@
-function VisibilityControl ({ setShowCompleted, cleanTask }) {
+
+function VisibilityControl ({ setShowCompleted, showCompleted, cleanTask }) {
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete it?')) {
       cleanTask()
@@ -7,11 +8,18 @@ function VisibilityControl ({ setShowCompleted, cleanTask }) {
   }
   return (
     <>
-      <div className='bg-gray-400 flex justify-evenly py-2'>
-        <label>
-          <input type='checkbox' onChange={() => setShowCompleted(a => !a)} />
-          Show Tasks Done
-        </label>
+      <div className='bg-gray-400 flex items-center justify-evenly py-2'>
+          
+            <label className='bg-gray-100 cursor-pointer relative w-10 h-5 rounded-full' >
+              <input 
+                  type='checkbox' 
+                  checked={showCompleted}
+                  onChange={() => setShowCompleted(a => !a)} 
+                  className='sr-only peer'
+              />
+              <span className='w-2/5 h-4/5 bg-blue-300 absolute rounded-full left-1 top-0.5 peer-checked:bg-blue-500 peer-checked:left-5 transition-all duration-500'></span>
+            </label>
+          <strong className='px-10 text-white'>Show Tasks Done</strong>
         <button className="bg-[#FF0000] hover:bg-red-600 text-white rounded-md px-2" onClick={handleDelete}>Clear</button>
       </div>
     </>
