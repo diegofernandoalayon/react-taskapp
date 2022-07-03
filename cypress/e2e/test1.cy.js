4a85c1a5e9d8d6575053ea53f5b073c40ca6a91b
Diego Fernando Alayon <dfar.41@gmail.com>
2022-07-02 21:53:15 -0500

    complentando test

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -3,8 +3,10 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
-    cy.get('form > input').should('be.visible').should('be.enabled').type('tarea 1{enter}')
-    cy.get('th').should('be.visible')
+    cy.get('form > input').should('be.visible').should('be.enabled').type('tarea 1{enter}').wait(200)
+    cy.get('th').should('be.visible').wait(200)
+    cy.get('.bg-slate-500 > .px-2 > .px-10').click().wait(200)
+    cy.get('.bg-gray-100').click().wait(200)
   })
 })
 
